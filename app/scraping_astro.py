7bfc19c3ec0bc53ba9f8354dad7db0e00a5c4f09
mr1451 <mr1451@georgetown.edu>
2020-05-04 15:24:01 -0400

    Most recent commits.

--- app/scraping_astro.py
+++ app/scraping_astro.py
@@ -2,11 +2,21 @@ import requests
 import json
 
 def zodiac_scrape(sign):
+    """
+    Takes user inputted astrological sign and compiles into request url.
+    Params:
+        Sign (string); one of 12 astrological signs determined by birthday
+    
+    Examples:
+        zodiac_scrape(Gemini)
+        zodiac_scrape(Taurus)
+    """
     request_url = f"http://horoscope-api.herokuapp.com/horoscope/today/{sign}"
     response = requests.get(request_url)
     parsed_response = json.loads(response.text)
     daily = parsed_response["horoscope"]
     print(daily)
+    exit()
     #request_url = f"http://horoscope-api.herokuapp.com/horoscope/week/{sign}"
     #response = requests.get(request_url)
     #parsed_response = json.loads(response.text)
---
+++ test/scraping_astro_test.py
@@ -0,0 +1,10 @@
+import os
+import pytest
+
+from app.scraping_astro import zodiac_scrape
+
+CI_ENV = os.environ.get("CI") == "true" # expect default environment variable setting of "CI=true" on Travis CI, see: https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+
+@pytest.mark.skipif(CI_ENV==True, reason="to avoid configuring credentials on, and issuing requests from, the CI server")
+
+#def test_zodiac_scrape():
