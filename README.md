cd1c185fd1cc5f08906d0bab11b714fb878b0432
mr1451 <mr1451@georgetown.edu>
2020-05-04 13:49:55 -0400

    Add files for testing and QC.

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 First, make sure to clone this repository to your own Github desktop. Once done, create a virtual enviroment with following code in the command line:
 
 ```sh
-conda create -n ssa-data-env python=3.7 # (first time only)
-conda activate ssa-data-env 
+conda create -n ssa-zodiac-env python=3.7 # (first time only)
+conda activate ssa-zodiac-env 
 ```
 
 From within the virtual environment, install the required packages specified in the "requirements.txt" file from within the repository: 
@@ -17,7 +17,7 @@ pip install -r requirements.txt
 
 ## Web App Usage
 
-Run the app:
+From within the virtual environment, demonstrate your ability to run the Python script from the command-line:
 
 ```py
 FLASK_APP=web_app flask run
--- web_app/routes/scraping_astro.py
+++ web_app/routes/scraping_astro.py
@@ -6,11 +6,11 @@ from flask import Blueprint, render_template, request
 
 #scraping_astro = Blueprint("scraping_astro", __name__)
 
-#@scraping_astro.route("/zodiacresult")
-#def zodiac_scrape(sign):
-#    request_url = f"http://horoscope-api.herokuapp.com/horoscope/today/{sign}"
-#    response = requests.get(request_url)
-#    parsed_response = json.loads(response.text)
-#    daily = parsed_response["horoscope"]
-#    print(daily)
-#    return render_template("zodiacresult.html", daily = daily)
+@zodiacresult_astro.route("/zodiacresult")
+def zodiac_scrape(sign):
+    request_url = f"http://horoscope-api.herokuapp.com/horoscope/today/{sign}"
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+    daily = parsed_response["horoscope"]
+    print(daily)
+    return render_template("zodiacresult.html", daily = daily)
