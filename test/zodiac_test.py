99169d658854eccb04601cc9e7c466e1fad8c49c
mr1451 <mr1451@georgetown.edu>
2020-05-05 18:22:08 -0400

    Update testing for zodiac functions.

--- test/zodiac_test.py
+++ test/zodiac_test.py
@@ -1,5 +1,7 @@
 import os
 import pytest
+import requests
+import json
 
 from app.zodiac import zodiac_scrape, zodiac_scrape_week, zodiac_scrape_month, zodiac_scrape_year
 
@@ -7,33 +9,54 @@ CI_ENV = os.environ.get("CI") == "true" # expect default environment variable se
 
 @pytest.mark.skipif(CI_ENV==True, reason="to avoid configuring credentials on, and issuing requests from, the CI server")
 
-def test_math():
-    assert 5 == 5
-
 def test_zodiac_scrape():
     sign = "Gemini"
 
-    parsed_response = zodiac_scrape(sign)
+    request_url = f"http://horoscope-api.herokuapp.com/horoscope/today/{sign}"
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+    zodiac_output = zodiac_scrape(sign)
 
-    assert isinstance(parsed_response, str)
-
+    assert parsed_response["sunsign"] == sign
+    assert "horoscope" in parsed_response
+    assert "sunsign" in parsed_response
+    assert isinstance(zodiac_output, str)
+    
 def test_zodiac_scrape_week():
     sign = "Gemini"
 
-    parsed_response = zodiac_scrape_week(sign)
+    request_url = f"http://horoscope-api.herokuapp.com/horoscope/week/{sign}"
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+    zodiac_output_week = zodiac_scrape_week(sign)
 
-    assert isinstance(parsed_response, str)
+    assert parsed_response["sunsign"] == sign
+    assert "horoscope" in parsed_response
+    assert "sunsign" in parsed_response
+    assert isinstance(zodiac_output_week, str)
 
 def test_zodiac_scrape_month():
     sign = "Gemini"
 
-    parsed_response = zodiac_scrape_month(sign)
+    request_url = f"http://horoscope-api.herokuapp.com/horoscope/month/{sign}"
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+    zodiac_output_month = zodiac_scrape_month(sign)
 
-    assert isinstance(parsed_response, str)
+    assert parsed_response["sunsign"] == sign
+    assert "horoscope" in parsed_response
+    assert "sunsign" in parsed_response
+    assert isinstance(zodiac_output_month, str)
 
 def test_zodiac_scrape_year():
     sign = "Gemini"
 
-    parsed_response = zodiac_scrape_year(sign)
+    request_url = f"http://horoscope-api.herokuapp.com/horoscope/year/{sign}"
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+    zodiac_output_year = zodiac_scrape_year(sign)
 
-    assert isinstance(parsed_response, str)
+    assert parsed_response["sunsign"] == sign
+    assert "horoscope" in parsed_response
+    assert "sunsign" in parsed_response
+    assert isinstance(zodiac_output_year, str)
