6e9f2e500406dffda0636cf8e774b2b3bf1f8ef6
mr1451 <mr1451@georgetown.edu>
2020-05-04 22:24:43 -0400

    Update home_routes.py

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -36,13 +36,16 @@ def about():
 #    output = ssa_scrape(name, sex)
 #    print(output)
 #    return render_template("result.html", ssa_output = ssa_output, name=name, sex=sex)
-    
+
 @home_routes.route("/result", methods=["POST"]) #https://www.youtube.com/watch?v=AEM8_4NBU04
 def pass_sign():
     sign = request.form["sign"]
     zodiac_output = zodiac_scrape(sign)
+    zodiac_output_week=zodiac_scrape_week(sign)
+    zodiac_output_month=zodiac_scrape_month(sign)
+    zodiac_output_year=zodiac_scrape_year(sign)
     name = request.form["name"]
     sex = request.form["sex"]
     ssa_output = ssa_scrape(name, sex)
     print (ssa_output)
-    return render_template("result.html", sign = sign, zodiac_output = zodiac_output, ssa_output = ssa_output, name=name, sex=sex )
+    return render_template("result.html", sign = sign, zodiac_output = zodiac_output, zodiac_output_week = zodiac_output_week, zodiac_output_month = zodiac_output_month, zodiac_output_year = zodiac_output_year, ssa_output = ssa_output, name=name, sex=sex )
